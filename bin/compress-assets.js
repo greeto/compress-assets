f837bc53033e58e1c3c2ea30c0d939ae2c1888f1
Tal Gerafi <gerafital2@gmail.com>
2018-01-11 15:35:20 +0200

    remove support for svgo - compress-images bug

--- bin/compress-assets.js
+++ bin/compress-assets.js
@@ -7,8 +7,6 @@ let path = require("path");
 if (path)
   path = path.resolve("./");
 
-console.log("HEYYYY");
-
 // If path not found, exit.
 if (!path) {
   console.error("Error: couldn't find current path.");
@@ -19,12 +17,11 @@ if (!path) {
 // Compressing
 var compress_images = require('compress-images'), INPUT_path_to_your_images, OUTPUT_path;
 
-INPUT_path_to_your_images = path + '/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+INPUT_path_to_your_images = path + '/**/*.{jpg,JPG,jpeg,JPEG,png,gif}';
 OUTPUT_path = path + '/compressed';
 
 compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: true}, false,
                                             {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
                                             {png: {engine: 'pngquant', command: ['--quality=20-50']}},
-                                            {svg: {engine: 'svgo', command: '--multipass'}},
                                             {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(){
 });
